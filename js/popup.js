20f44bc80322ea6fe5d2aae67cfe541407b09893
Skaper777 <skaper777@gmail.com>
2018-08-13 16:34:56 +0300

    popup close

--- js/popup.js
+++ js/popup.js
@@ -2,32 +2,37 @@
 
 (function () {
   var overlays = document.querySelectorAll('.slider__slide-overlay');
-  var overlayPopups = document.querySelectorAll('.popup__image');   
+  var popup = document.querySelector('.popup');
+  var popupImages = document.querySelectorAll('.popup__image');
+  var popupClose = document.querySelector('.popup__close');       
   
   var openPopup = function (e) {    
     var overlay = e.target;    
     for (var i = 0; i < overlays.length; i++) {
       if (overlay === overlays[i]) {
-        overlayPopups[i].style = 'display: block';
+        popup.style = 'display: block';        
+        popupImages[i].style = 'display: block';
       }
     }
   };  
 
   var closePopup = function (e) {
-    var overlayPopup = e.target;    
-    for (var i = 0; i < overlayPopups.length; i++) {
-      if (overlayPopup === overlayPopups[i]) {
-        overlayPopups[i].style = 'display: none';
+    var target = e.target;    
+    for (var i = 0; i < popupImages.length; i++) {
+      if (target === popupClose) {
+        popupImages[i].style = 'display: none';
       }
     }
+
+    popup.style = 'display: none';
   }; 
   
   var popupHandler = function () {
     for (var j = 0; j < overlays.length; j++) {         
-      overlays[j].addEventListener('click', openPopup);
-      overlayPopups[j].addEventListener('click', closePopup);    
+      overlays[j].addEventListener('click', openPopup);          
     }
   };
 
+  popupClose.addEventListener('click', closePopup);
   popupHandler();
 })();
