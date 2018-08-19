e51f62c4583af71de5071dfb25595597fa14cdf0
Skaper777 <skaper777@gmail.com>
2018-08-19 17:51:15 +0300

    list style

---
+++ js/hovers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+  var spans = document.querySelectorAll('.services__span');
+  var svgs = document.querySelectorAll('.services__svg');
+  
+  for (var i = 0; i < spans.length; i++) {
+    var span = spans[i];
+    var svg = svgs[i];
+  }
+})();
